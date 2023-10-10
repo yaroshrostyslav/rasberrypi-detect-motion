41bf2f6e1b1ecf30c02aba59b5f1dd7b62b6959a
rost <yaroshrostyslav@gmail.com>
2023-10-10 10:10:32 +0300

    init project

---
+++ api/test.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Start PHP script..." . PHP_EOL;
+
+//use CV\Scalar, CV\Size;
+//use function CV\{imread, imwrite, rectangle};
+//
+//
+//$src = imread("images/faces.jpg");
+//$size = $src->size(); // 2000x500
+//
+//
+//var_dump($size);
+while (true){
+//    unlink('1.jpg');
+//    exec('ffmpeg -f v4l2 -i /dev/video0 -vframes 1 -y 1.jpg');
+//    sleep(1);
+}
